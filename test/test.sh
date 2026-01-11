8cafe5c1fc29a236f8b8dc97b144cb2779ea0849
Torsten Seemann <torsten.seemann@gmail.com>
2026-01-12 09:43:08 +1100

    Naive but functional tool

--- test/test.sh
+++ test/test.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bats
 
 setup () {
+  name="ziggypep"
   bats_require_minimum_version 1.5.0
   dir=$(dirname "$BATS_TEST_FILENAME")
   cd "$dir"
-  exe="$dir/../bin/ziggypep"
+  exe="$dir/../bin/$name"
 }
+
 @test "Script syntax check" {
   run -0 perl -c "$exe"
 }
 @test "Version" {
   run -0 $exe -v
-  [[ "$output" =~ "ziggypep" ]]
+  [[ "$output" =~ "$name " ]]
 }
 @test "Help" {
   run -0 $exe -h
@@ -27,11 +29,34 @@ setup () {
 @test "Bad option" {
   run ! $exe --doesnotexist
 }
-@test "Normal input" {
-  run -0 $exe prot.faa
-  [[ "$output" =~ "NIRS_PSEST" ]]
+@test "Null input" {
+  run ! $exe /dev/null
+  [[ "$output" =~ "ERROR" ]]
+}
+@test "Uncompressed input" {
+  run -0 $exe pos.faa
+  [[ "$output" =~ "PHL2_BACCE" ]]
+  [[ ! "$output" =~ "WARNING" ]]
 }
 @test "Gzipped input" {
-  run -0 $exe prot.faa
-  [[ "$output" =~ "NIRS_PSEST" ]]
+  run -0 $exe test.faa.gz
+  [[ "$output" =~ "WP_016198974.1" ]]
+  [[ "$output" =~ "has non" ]]
+}
+@test "Bzipped input" {
+  run -0 $exe neg.faa.bz2
+  [[ "$output" =~ "CYOD_ECOLI" ]]
+  [[ "$output" =~ "does not" ]]
+}
+@test "STDIN input" {
+  run -0 $exe /dev/stdin < neg.faa.bz2
+  [[ "$output" =~ "is $name" ]]
+}
+@test "Quiet mode -q" {
+  run -0 $exe -q pos.faa
+  [[ ! "$output" =~ "is $name" ]]
+}
+@test "Signals only -s" {
+  run -0 $exe -s pos.faa
+  [[ ! "$output" =~ "LSHR_RAT" ]]
 }
